df639a1f33dfc530b66de6a32c78ad337e002355
Lorenzo Zanetti <lzanetti@ikp.tu-darmstadt.de>
2018-02-10 11:39:19 +0100

    writing of evtfunnel.c continues

--- multisim/evtfunnel.c
+++ multisim/evtfunnel.c
@@ -23,10 +23,11 @@ typedef struct event{
 //functions:
 int merge( evt *merged, const evt *one );
 int load_horizontal( evt **evarr, FILE **streams );
+void put_event( FILE *stream, evt *event );
 
 evt *evt_alloc( int nb_lines );
+void evt_resize( evt *given, int sz );
 void evt_free( evt *event );
-int evt_settitle( evt *event, const char *firstline );
 int evt_push( evt *event, const char *line );
 
 //------------------------------------------------------------------------------------
@@ -68,7 +69,7 @@ int main( int argc, char **argv ){
 		memset( in_fname[in_fcount-1], 0, 256 );
 	}
 	
-	FILE *in_file[64];
+	FILE *in_file[MAX_FILES]; memset( in_file, 0, sizeof(FILE*) );
 	for( i=0; i < in_fcount; ++i ){
 		in_file[i] = fopen( in_fname[i], "r" );
 		if( !in_file[i] ){
@@ -90,8 +91,9 @@ int main( int argc, char **argv ){
 	int nb_evt = 0;
 	do{
 		nb_evt = load_horizontal( evarr, in_file );
-		//merge all events and then put it on the output stream.
-		//...
+		for( i=0; i < nb_evt-1; ++i )
+			merge( evarr[i+1], evarr[i] ); //the merged event is left in the last place
+		put_event( out_file, evarr[nb_evt-1];
 	} while( nb_evt );
 	
 	return 0;
@@ -99,3 +101,43 @@ int main( int argc, char **argv ){
 
 //------------------------------------------------------------------------------------
 //LOTS of functions!!!
+
+//------------------------------------------------------------------------------------
+//merge two events
+int merge( evt *merged, const evt *one ){
+	int n_tracks = 0, evt_id = 0, i;
+	float b_mass, b_beta;
+	sscanf( one->firstline, "%d\t%d\t%f\t%f", &evt_id, &n_tracks, &b_mass, &b_beta );
+	for( i=0; i < n_tracks; ++i ) evt_push( merged, one->lines[i] );
+	sprintf( merged->firstline, "%d\t%d\t%f\t%f", evt_id, merged->nl, b_mass, b_beta );
+	return merged->nl;
+}
+
+//------------------------------------------------------------------------------------
+//load horizontally the events (one for each file), until files are available.
+int load_horizontal( evt **evarr, FILE **streams ){
+	int i, t, id, nb_loaded=0;
+	char cbuf[LINESZ];
+	for( i=0; i < MAX_FILES; ++i ){
+		if( !streams[i] ) continue; //it's critical that unused files are set to NULL!
+		fgetc( streams[i] );
+		if( feof( streams[i] ) ){
+			fclose( streams[i] );
+			streams[i] = NULL;
+			continue;
+		}
+		fungetc( streams[i] );
+		
+		fgets( evarr[i]->firstline, LINESZ, streams[i] );
+		sscanf( evarr[i]->firstline, "%d\t%d", &id, &evarr[i]->nl );
+		evt_resize( evarr[i], evarr[i]->nl );
+		for( t=0; t < evarr[i]->nl; ++t )
+			fgets( evarr[i]->lines[t], LINESZ, streams[i] );
+		nb_loaded++;
+	}
+	
+	return nb_loaded;
+}
+			
+	
+	
